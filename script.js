fd07e01004d729ff33b798fea787794eb3e5edad
HaneenIbrahem <haneen.m.ibrahem@gmail.com>
2024-06-06 21:58:37 +0300

    some modifications

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var paragraph = document.querySelector("p");
 var heading = document.querySelector("h1");
 var changeSizeBtn = document.querySelector("button");
+// var div = parentDiv = document.querySelector("div");
 
 function changeSize(){
     paragraph.classList.toggle("changeFontSize")
     heading.classList.toggle("changeFontSize")
+    // parentDiv.classList.toggle("changeFontSize")
 }
 
 changeSizeBtn.onclick = changeSize;
